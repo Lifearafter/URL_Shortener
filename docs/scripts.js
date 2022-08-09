573b865ebff0972b5e98a525cada3b483b160619
Solomon Pierce <1nepthys1@gmail.com>
2022-08-09 11:57:09 -0500

    Refactor: replace var with let and const

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,39 +1,39 @@
-var urltoapi = "https://nxihka4eoi.execute-api.us-east-1.amazonaws.com/dev"
-var shortmainurl;
+const API_URL = "https://nxihka4eoi.execute-api.us-east-1.amazonaws.com/dev"
+let output_url = '';
 
 function inputButtonClick() {
 
-    var longurl = document.getElementById("inputtext").value;
+    const shorturl = document.getElementById("inputtext").value;
     
-    if (longurl == "" || isValidURL(longurl) == false || isNotAPIredirect(longurl) == true) {
+    if (shorturl == "" || isValidURL(shorturl) == false || isNotAPIredirect(shorturl) == true) {
         alert("Please enter a valid input");
     }
     else {
-        var url = urltoapi + "/url?long_url=" + longurl;
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", url, true);
+        const xhr_request_url = API_URL + "/url?shorturl=" + shorturl;
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", xhr_request_url, true);
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4) {
                 if(xhr.status == 200){
-                    var response = xhr.responseText;
-                    var json = JSON.parse(response);
-                    var output = json.short_url;
-                    shortmainurl = urltoapi+'/'+output;
-                    document.getElementById("inputtext").value = shortmainurl;
+                    const response = xhr.responseText;
+                    const json = JSON.parse(response);
+                    const output = json.longurl;
+                    output_url = API_URL+'/'+output;
+                    document.getElementById("inputtext").value = output_url;
                     showRedirectDiv();
                 }
                 else if (xhr.status == 404) {
-                    var req = new XMLHttpRequest();
-                    url = urltoapi + "/add?long_url=" + longurl;
-                    req.open("POST", url, true);
+                    let req = new XMLHttpRequest();
+                    xhr_request_url = API_URL + "/add?long_url=" + shorturl;
+                    req.open("POST", xhr_request_url, true);
                     req.onreadystatechange = function () {
                         if (req.readyState == 4) {
                             if (req.status == 200) {
-                                var response = req.responseText;
-                                var json = JSON.parse(response);
-                                var output = json.short_url;
-                                shortmainurl = urltoapi+'/'+output;
-                                document.getElementById("inputtext").value = shortmainurl;
+                                const response = req.responseText;
+                                const json = JSON.parse(response);
+                                const output = json.longurl;
+                                output_url = API_URL+'/'+output;
+                                document.getElementById("inputtext").value = output_url;
                                 showRedirectDiv();
                             }
                             else {
@@ -44,7 +44,7 @@ function inputButtonClick() {
                     req.send();
                 }
                 else{
-                    var response = JSON.parse(xhr.responseText);
+                    const response = JSON.parse(xhr.responseText);
                     document.getElementById("inputtext").value = response.message;
                 }
             }
@@ -54,21 +54,20 @@ function inputButtonClick() {
 }
 
 function inputRedirect(){
-    window.open(shortmainurl, '_blank');
-    let redirectDiv = document.getElementById("redirectDiv");
+    window.open(output_url, '_blank');
+    const redirectDiv = document.getElementById("redirectDiv");
     redirectDiv.style.display = "none";
 }
+
 function showRedirectDiv(){
-    let redirectDiv = document.getElementById("redirectDiv");
+    const redirectDiv = document.getElementById("redirectDiv");
     redirectDiv.style.display = "block";
 }
 
 function isValidURL(string) {
-    var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    return (res !== null)
+    return null !== string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
 }
 
 function isNotAPIredirect(string){
-    var res = string.includes(urltoapi);
-    return res;
+    return string.includes(API_URL);
 }
